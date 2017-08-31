24d06fe0e6c5442b6f381c82c2f03ebfb869d305
Roman <rromaromik@gmail.com>
2017-08-31 03:06:48 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+
+    // slider
+    $('.slider').slick({
+        arrows: true,
+        slidesToShow: 1,
+        speed: 700,
+    });
+
+
+    // active
+    $('.rate-list li').click(function() {
+        if (!$(this).hasClass('active')) {
+            $('.rate-list li').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+
+
+
+    $('.rates-right a').click(function() {
+        if (!$(this).hasClass('active')) {
+            $('.rates-right a').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+
+
+
+});
+
