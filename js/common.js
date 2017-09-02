ec3d81b1198a76d9d908e738abc8ab4ff712537e
Roman <rromaromik@gmail.com>
2017-09-02 16:29:54 +0300

    rebuild

--- js/common.js
+++ js/common.js
@@ -25,11 +25,44 @@ $(document).ready(function() {
             $(this).addClass('active');
         }
     });
+    // $('.service-check').click(function () {
+    //     $('.service-check').toggleClass('checked');
+    // });
+    //
+    // var showTopService = (function () {
+    //     $('.service-check').on('click', function () {
+    //         $(this).parents('.Tservices-item').toggleClass('active');
+    //         $(this).parent().siblings('.Tservices-item').removeClass('active');
+    //     });
+    // });
+    // showTopService();
+    function checkService() {
+        $('.service-check').each(function() {
+            var $this = $(this),
+                $chekbox = $this.find('input[type="checkbox"]');
+            if (!$($chekbox).hasClass("patched4")) $chekbox.unbind().on('change', function() {
+                if ($(this).is(':checked')) {
+                    $(this).prop('checked', true);
+                } else {
+                    $(this).prop('checked', false);
+                }
+                $(".tariff-list").stop().fadeOut();
+                is_($this);
+            }).addClass("patched4");
+            is_($this);
+        });
 
-    $('.form-selection label').click(function () {
-        $(this).parents('.select').toggleClass('checked');
-    });
-
+        function is_(elem) {
+            var $elem = $(elem),
+                $input = $elem.find('input[type="checkbox"]');
+            if ($input.is(':checked')) {
+                $elem.addClass('checked');
+            } else {
+                $elem.removeClass('checked');
+            }
+        }
+    }
+    checkService();
 
 });
 
